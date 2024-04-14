15535688cba55f28ed0071b1bb3b06cb6da71453
Keyukemi <keyukemi.ku@gmail.com>
2024-04-14 12:02:34 +0100

    added port

--- app/main.py
+++ app/main.py
@@ -34,3 +34,8 @@ app.include_router(comments.router)
 @app.get("/")
 def root(): 
     return {"message": "hello world"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
